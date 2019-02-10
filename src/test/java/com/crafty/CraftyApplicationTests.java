93baeade5c56641de8fdc27dbc6b90a1a9822dcd
Meri-em <maria.karcheva@yahoo.com>
2019-02-10 18:16:38 +0200

    initialize Spring Boot project

---
+++ src/test/java/com/crafty/CraftyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.crafty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CraftyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
