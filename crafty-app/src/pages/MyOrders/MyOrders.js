b3579cd4c6e56b52d817a238617f3f2d983613e0
IliaSky <iliaskywalker@gmail.com>
2019-08-31 18:13:58 +0300

    FE update MyOrders to match BE.

--- crafty-app/src/pages/MyOrders/MyOrders.js
+++ crafty-app/src/pages/MyOrders/MyOrders.js
@@ -9,23 +9,36 @@ class MyOrders extends Component {
   componentDidMount() {
     getOrders().then(({data}) => this.setState({data}));
   }
+  renderOrder = ({total, items, createdAt}, i) => (
+    <div className="order" key={i}>
+      <span className="date">{new Date(createdAt.replace(/\..*/, '')).toLocaleString()}</span>
+      <div className="item-list">
+        {items.map(({item, paid, quantity}) => (
+          <div className="item" key={item.id}>
+            <strong className="item-name">{item.name}</strong>
+            <img className="item-img" src={item.image} alt={item.name}/>
+            <span className="item-price">{quantity > 1 ? quantity + ' x ': ''}{paid}</span>
+          </div>
+        ))}
+      </div>
+      <span className="total">Общо: {total}</span>
+    </div>
+  );
   render() {
+    const { sales=[], purchases=[] } = this.state.data;
     // return <pre className="MyOrders">{JSON.stringify(this.state.data, null, 2)}</pre>
-    return <ul className="MyOrders">{this.state.data.map(({total, items, createdAt}, i) => (
-      <li className="order" key={i}>
-        <span className="date">{new Date(createdAt.replace(/\..*/, '')).toLocaleString()}</span>
-        <div className="item-list">
-          {items.map(({item, paid, quantity}) => (
-            <div className="item" key={item.id}>
-              <strong className="item-name">{item.name}</strong>
-              <img className="item-img" src={item.image} alt={item.name}/>
-              <span className="item-price">{quantity > 1 ? quantity + ' x ': ''}{paid}</span>
-            </div>
-          ))}
+    return (
+      <div className="MyOrders">
+        <div className="MyPurchases">
+          <h2>Покупки</h2>
+          {purchases.map(this.renderOrder)}
         </div>
-        <span className="total">Общо: {total}</span>
-      </li>
-    ))}</ul>
+        <div className="MySales">
+          <h2>Продажби</h2>
+          {sales.map(this.renderOrder)}
+        </div>
+      </div>
+    )
   }
 }
 
